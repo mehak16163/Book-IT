6f595005075c424dc5efacca8dbcfef9d6896593
pankhuri kasliwal <pankhuri16253@iiitd.ac.in>
2017-11-14 10:59:25 +0530

    changes

--- src/CRBSystem/Admin_Intro/Respond_signup_controller.java
+++ src/CRBSystem/Admin_Intro/Respond_signup_controller.java
@@ -15,7 +15,23 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+
+/**
+*
+* <h1> Respond to SignUp Requests</h1>
+* <p>This class basically contains methods and functions that help the admin to react to signup requests ,accept them or reject as well as show the details of various people who have signed up.</p>
+*
+*
+* @author Mehak Gupta
+* @author Pankhuri Kasliwal
+* @version 1.0
+* @since October15,2017
+*
+*
+*/
+
 public class Respond_signup_controller {
+
 	@FXML
 	private ListView lw;
 	private ObservableList<String> list = FXCollections.observableArrayList();
@@ -24,6 +40,14 @@ public class Respond_signup_controller {
 	@FXML
 	private TextField ta;
 	@FXML
+
+	/**
+	* <p> This method is used to populate the requests send by various people who are willing to sign up on bookit</p> 
+	* @throws FileNotFoundException
+	* @throws ClassNotFoundException
+	* @throws IOException If an input or output exception occured
+	*
+	*/
 	private void populate() throws FileNotFoundException, ClassNotFoundException, IOException {
 		
 		File file = new File("./src/Database/signup");
@@ -36,9 +60,17 @@ public class Respond_signup_controller {
 		if(list.isEmpty()) {
 			return;
 		}
-		//System.out.println(list.get(0));
+		System.out.println(list.get(0));
 		lw.setItems(list);
 	}
+
+	/**
+	*
+	* <p>This method is for the admin that helps him to respond to the requests sent </p>
+	* @throws FileNotFoundException
+	* @throws ClassNotFoundException
+	* @throws IOException If an input or output exception occured
+	*/
 	@FXML
 	private void Accept() throws FileNotFoundException, ClassNotFoundException, IOException {
 		String name = tf.getText();
@@ -66,6 +98,15 @@ public class Respond_signup_controller {
 			}
 		}
 	}
+
+	/**
+	*
+	* <p>This method shows the details of various people who signed up.</p>
+	*
+	* @throws FileNotFoundException
+	* @throws ClassNotFoundException
+	* @throws IOException If an input or output exception occured
+	*/
 	@FXML
 	private void ShowDetails() throws FileNotFoundException, ClassNotFoundException, IOException {
 		String name = tf.getText();
@@ -79,10 +120,15 @@ public class Respond_signup_controller {
 			if(name.equals(st.getName())) {
 				ta.setText(st.getName() +" " + st.getEmail() +" " + st.getType() +" " + st.getPass());
 				break;
-
 			}
+			
 		}
 	}
+	/**
+	*
+	* <p> back button </p>
+	* @throws IOException If an input or output exception occured
+	*/
 	@FXML
 	private void back() throws IOException {
 		Main.showAdminIntro();
